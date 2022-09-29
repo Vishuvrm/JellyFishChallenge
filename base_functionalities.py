c57eebe34fa09e22f235c2471b85d04cc9e8b0d9
Vishal <vishuvrm96@gmail.com>
2022-09-29 09:50:10 +0530

    Initial commit

--- base_functionalities.py
+++ base_functionalities.py
@@ -2,19 +2,20 @@ from abc import ABC, abstractmethod
 
 
 class JellyFishOps(ABC):
-    """Operations"""
+    """Operations which are needed to be performed on the jellyfish"""
     @abstractmethod
     def operation(self):
       pass
 
 class GetDirectionRules(ABC):
+  """Direction rules which can be unique for each operation"""
   @property
   @abstractmethod
   def direction_rule(self):
       pass
 
 class Result(ABC):
-    '''Result'''
+    """Result for any operation"""
     @abstractmethod
     def apply(*args, ops, **kwargs):
         pass
