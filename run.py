d2ff80564f147a3c2017b5bddc14c9bf397646e6
Vishal <vishuvrm96@gmail.com>
2022-09-29 11:18:05 +0530

    Added some documentations and comments

--- run.py
+++ run.py
@@ -11,5 +11,6 @@ if __name__ == "__main__":
 11E RFRFRFRF
 32N FRRFLLFFRRFLL
 03W LLFFFLFLFL"""
-  # We can add more functionalities here in the similar way by importing them from operations module...
+  # To show the final result using GetMultipleJellyFishPos when each fish position is being calculated by GetFinalPosition
   ResultantPositions.apply(inputs, operation=GetMultipleJellyFishPos, using=GetFinalPosition)
+  # We can add more functionalities here in the similar way by importing them from operations module...
